8ad304625e7a27f1e5fe673fc5c4db0803193b49
taewan <taewan0530@daum.net>
2017-05-06 13:14:02 +0900

    SlideShowViewController 생성.

---
+++ FlickrSlideShow/Controller/SlideShowViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SlideShowViewController.swift
+//  FlickrSlideShow
+//
+//  Created by taewan on 2017. 5. 6..
+//  Copyright © 2017년 taewan. All rights reserved.
+//
+
+import UIKit
+
+final class SlideShowViewController: BaseViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
